6e2577fd8c6c9a9702666f5df557ea9b665dea3d
Santo Shakil <shakil.mezbah@gmail.com>
2023-03-08 20:25:35 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# embadded_pocketbase_flutter
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final pb = await initPB();
+  if (pb != null) {
+    final result = await Process.run(pb.path, ['--version']);
+    debugPrint('Execution Result: ${result.stdout}');
+  }
+  runApp(const MainApp());
+}
+
+Future<File?> initPB() async {
+  try {
+    final dir = join((await getApplicationDocumentsDirectory()).path, 'pocketbase');
+    final pbFile = File(join(dir, 'pocketbase'));
+    // pbFile.deleteSync();
+    if (pbFile.existsSync()) return pbFile;
+    final pbAsset = await rootBundle.load('assets/pocketbase');
+    await pbFile.create(recursive: true);
+    await pbFile.writeAsBytes(pbAsset.buffer.asUint8List());
+    pbFile.setExecutable(true);
+    return pbFile;
+  } catch (e) {
+    debugPrint('Error: $e');
+  }
+  return null;
+}
+
+extension on File {
+  Future<void> setExecutable(bool executable) async {
+    late final ProcessResult result;
+    if (Platform.isWindows) {
+      await Process.run('icacls', [path, '/inheritance:r']);
+      result = await Process.run('icacls', [path, '/grant:r', 'Users:F']);
+    } else {
+      result = await Process.run('chmod', [executable ? '+x' : '-x', path]);
+    }
+    debugPrint('Set Executable Result: ${result.stderr}');
+  }
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
